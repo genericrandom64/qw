2c19a17ca81fd9d7ffdc721484f3165624c1d7d0
Nishant Chatterjee <nishantc1527@gmail.com>
2020-08-07 13:50:48 -0700

    Just Cleaned Up Stuff

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include <string.h>
 #include <stdio.h>
-#include <stdlib.h> // this is important for political reasons
-int main( int argc, char *argv[] ) {
-    if(argc == 3) {
+
+int main(int argc, char *argv[]) {
+    argc--;
+    argv++;
+
+    if (argc == 2) {
         char new[65535] = "\n";
-        strcat(argv[2], new);
+        strcat(argv[1], new);
         FILE *door;
-        door = fopen(argv[1],"a+");
-        fprintf(door,"%s",argv[2]);
+        door = fopen(argv[0], "a+");
+        fprintf(door, "%s", argv[1]);
         fclose(door); // you better believe i changed every instance of the variable to door just for the joke
-        exit(0); // eorks
-    }
-    if(argc != 3) {
-        printf("Usage:\n\n    qw [file to write to] [text to write]\n\nQW writes a string to a file quickly, followed by a newline.\nWritten by genericrandom64 for the Shitty Software Suite\n");
+    } else {
+        printf(
+            "Usage:\n\n    qw [file to write to] [text to write]\n\nQW writes a string to a file quickly, followed by a newline.\nWritten by genericrandom64 for the Shitty Software Suite\n");
     }
 }
