1af97bf7e3d1fd0f79f886e3f2d295c63741794a
Nishant Chatterjee <nishantc1527@gmail.com>
2020-08-17 12:37:03 -0700

    worked magic

--- main.c
+++ main.c
@@ -1,17 +1,29 @@
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
     argc--;
     argv++;
 
     if (argc == 2) {
-        char new[65535] = "\n";
-        strcat(argv[1], new);
-        FILE *door;
-        door = fopen(argv[0], "a+");
-        fprintf(door, "%s", argv[1]);
-        fclose(door); // you better believe i changed every instance of the variable to door just for the joke
+        char* echo = "echo ";
+        char* quote1 = "\'";
+        char* text = argv[1];
+        char* quote2 = "\'";
+        char* arrow = " >> ";
+        char* file = argv[0];
+
+        char* command = malloc(strlen(echo) + strlen(quote1) + strlen(text) + strlen(quote2) + strlen(arrow) + strlen(file) + 1);
+
+        strcat(command, echo);
+        strcat(command, quote1);
+        strcat(command, text);
+        strcat(command, quote2);
+        strcat(command, arrow);
+        strcat(command, file);
+
+        popen(command, "w");
     } else {
         printf(
             "Usage:\n\n    qw [file to write to] [text to write]\n\nQW writes a string to a file quickly, followed by a newline.\nWritten by genericrandom64 for the Shitty Software Suite\n");
